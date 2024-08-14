397f40bf2cec73b3639fbc58ab6a768d260457a0
70725 <70725@selwyn.school.nz>
2024-08-15 09:40:10 +1200

    Trying to add autocomplete options for searchbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+// Autocomplete 
+let availableKeywords = [
+    'HTML',
+    'CSS',
+    'Easy Tutorials',
+    'Web design tutorials',
+    'Javascript',
+    'Where to learn coding online',
+    'Where to learn web design',
+    'How to create a website',
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputBox = document.getElementById("input-box");
+
+inputBox.onkeyup = function () {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length) {
+        result = availableKeywords.filter((keyword) => {
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);
+    }
+    display(result)
+}
+
+function display(result) {
+    const content = result.map((list) => {
+        return "<li>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content + "</ul>";
+}
+
+
+
 // FAQ Accordian
 document.addEventListener('DOMContentLoaded', () => {
     const faqContainer = document.querySelector('.faq-content');
